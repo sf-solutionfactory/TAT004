2aba6f82550bf6bbfc71f1793326e5d04e71916e
Rogelio Sánchez Gómez <36237248+sf-rogeliosanchez@users.noreply.github.com>
2018-03-09 10:42:03 -0600

    Carta Fija
    Formulario para carta fija

--- TAT001/Controllers/PresupuestoController.cs
+++ TAT001/Controllers/PresupuestoController.cs
@@ -37,15 +37,20 @@ namespace TAT001.Controllers
         [Authorize]
         public ActionResult Carga()
         {
+            int pagina = 101; //ID EN BASE DE DATOS
             using (TAT001Entities db = new TAT001Entities())
             {
                 string u = User.Identity.Name;
+                //string u = "admin";
                 var user = db.USUARIOs.Where(a => a.ID.Equals(u)).FirstOrDefault();
                 ViewBag.permisos = db.PAGINAVs.Where(a => a.ID.Equals(user.ID)).ToList();
                 ViewBag.carpetas = db.CARPETAVs.Where(a => a.USUARIO_ID.Equals(user.ID)).ToList();
-                ViewBag.nombre = user.NOMBRE + " " + user.APELLIDO_P + " " + user.APELLIDO_M;
-                ViewBag.email = user.EMAIL;
+                ViewBag.usuario = user;
                 ViewBag.rol = user.MIEMBROS.FirstOrDefault().ROL.NOMBRE;
+                ViewBag.Title = db.PAGINAs.Where(a => a.ID.Equals(pagina)).FirstOrDefault().PAGINATs.Where(b => b.SPRAS_ID.Equals(user.SPRAS_ID)).FirstOrDefault().TXT50;
+                ViewBag.warnings = db.WARNINGVs.Where(a => (a.PAGINA_ID.Equals(pagina) || a.PAGINA_ID.Equals(0)) && a.SPRAS_ID.Equals(user.SPRAS_ID)).ToList();
+                ViewBag.textos = db.TEXTOes.Where(a => (a.PAGINA_ID.Equals(pagina) || a.PAGINA_ID.Equals(0)) && a.SPRAS_ID.Equals(user.SPRAS_ID)).ToList();
+
                 try
                 {
                     string p = Session["pais"].ToString();
@@ -53,10 +58,46 @@ namespace TAT001.Controllers
                 }
                 catch
                 {
+                    //ViewBag.pais = "mx.svg";
                     return RedirectToAction("Pais", "Home");
                 }
+                Session["spras"] = user.SPRAS_ID;
             }
             return View();
         }
+        [HttpPost]
+        public ActionResult Carga(HttpPostedFileBase file)
+        {
+            int pagina = 102; //ID EN BASE DE DATOS
+            using (TAT001Entities db = new TAT001Entities())
+            {
+                string u = User.Identity.Name;
+                //string u = "admin";
+                var user = db.USUARIOs.Where(a => a.ID.Equals(u)).FirstOrDefault();
+                ViewBag.permisos = db.PAGINAVs.Where(a => a.ID.Equals(user.ID)).ToList();
+                ViewBag.carpetas = db.CARPETAVs.Where(a => a.USUARIO_ID.Equals(user.ID)).ToList();
+                ViewBag.usuario = user;
+                ViewBag.rol = user.MIEMBROS.FirstOrDefault().ROL.NOMBRE;
+                ViewBag.Title = db.PAGINAs.Where(a => a.ID.Equals(pagina)).FirstOrDefault().PAGINATs.Where(b => b.SPRAS_ID.Equals(user.SPRAS_ID)).FirstOrDefault().TXT50;
+                ViewBag.warnings = db.WARNINGVs.Where(a => (a.PAGINA_ID.Equals(pagina) || a.PAGINA_ID.Equals(0)) && a.SPRAS_ID.Equals(user.SPRAS_ID)).ToList();
+                ViewBag.textos = db.TEXTOes.Where(a => (a.PAGINA_ID.Equals(pagina) || a.PAGINA_ID.Equals(0)) && a.SPRAS_ID.Equals(user.SPRAS_ID)).ToList();
+
+                try
+                {
+                    string pa = Session["pais"].ToString();
+                    ViewBag.pais = pa + ".svg";
+                }
+                catch
+                {
+                    //ViewBag.pais = "mx.svg";
+                    return RedirectToAction("Pais", "Home");
+                }
+                Session["spras"] = user.SPRAS_ID;
+                return View();
+            }
+        }
+
+
+
     }
 }
---
+++ TAT001/Models/CartaV.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TAT001.Models
+{
+    public class CartaV
+    {
+        public string company { get; set; }
+        public bool company_x { get; set; }
+
+        public string taxid { get; set; }
+        public bool taxid_x { get; set; }
+
+        public string concepto { get; set; }
+        public bool concepto_x { get; set; }
+
+        public string cliente { get; set; }
+        public bool cliente_x { get; set; }
+
+        public string puesto { get; set; }
+        public bool puesto_x { get; set; }
+
+        public string direccion { get; set; }
+        public bool direccion_x { get; set; }
+
+        public string folio { get; set; }
+        public bool folio_x { get; set; }
+
+        public string lugar { get; set; }
+        public bool lugar_x { get; set; }
+
+        public string payer { get; set; }
+        public bool payer_x { get; set; }
+
+        public string estimado { get; set; }
+        public bool estimado_x { get; set; }
+
+        public string mecanica { get; set; }
+        public bool mecanica_x { get; set; }
+
+        public string nombreE { get; set; }
+        public bool nombreE_x { get; set; }
+
+        public string puestoE { get; set; }
+        public bool puestoE_x { get; set; }
+
+        public string companyC { get; set; }
+        public bool companyC_x { get; set; }
+
+        public string nombreC { get; set; }
+        public bool nombreC_x { get; set; }
+
+        public string puestoC { get; set; }
+        public bool puestoC_x { get; set; }
+
+        public string companyCC { get; set; }
+        public bool companyCC_x { get; set; }
+
+        public string legal { get; set; }
+        public bool legal_x { get; set; }
+
+        public string mail { get; set; }
+        public bool mail_x { get; set; }
+
+        public string comentarios { get; set; }
+        public bool comentarios_x { get; set; }
+
+        public string compromisoK { get; set; }
+        public bool compromisoK_x { get; set; }
+
+        public string compromisoC { get; set; }
+        public bool compromisoC_x { get; set; }
+
+        //TABLA DE MATERIALES O CATEGORÍAS
+
+    }
+}
